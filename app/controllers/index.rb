1973be14e9408979f499624d960fa8efb15ab31f
Navid Mostafavi <navidm@gmail.com>
2013-06-30 21:40:40 -0700

    crud working

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,60 @@
+
+
 get '/' do
   # Look in app/views/index.erb
+  @notes = Note.all
   erb :index
 end
+
+get '/create' do
+  erb :create_note
+
+end
+
+post '/create' do
+
+  Note.create(params)
+  @notes = Note.all
+
+  erb :index
+end
+
+get '/read/:id' do
+
+  @one_note = Note.find(params[:id])
+  erb :read_note
+
+end
+
+get '/update/:id' do
+
+  @note = Note.find(params[:id])
+  erb :update_note
+
+end
+
+post '/update' do
+
+  note = Note.find(params[:id])
+  note.title = params[:title]
+  note.content = params[:content]
+  note.save
+  @notes = Note.all
+
+  erb :index
+
+end
+
+
+get '/delete/:id' do
+
+  Note.destroy(params[:id])
+  @notes = Note.all
+
+  # if Note.empty?
+  #   Note.reset_pk_sequence
+  # end
+  erb :index
+
+end
+
---
+++ app/models/note.rb
@@ -0,0 +1,7 @@
+# Simple table that represents the roll of a 6-sided die
+
+class Note < ActiveRecord::Base
+
+
+
+end
